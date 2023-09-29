04adfd9a5ed84ff5c204488db491a1f1acb8f3fd
sambidhansenapati024 <sambidhansenapati208@gmail.com>
2023-09-29 18:21:30 +0530

    this is a game of brick and ball

---
+++ Main.java
@@ -0,0 +1,14 @@
+import javax.swing.JFrame;
+
+public class Main{
+    public static void main(String[] args) {
+        JFrame obj = new JFrame();
+        Gameplay gamePlay = new Gameplay();
+        obj.setBounds(10, 10, 700, 600);
+        obj.setTitle("Gudu's Ball Game");
+        obj.setResizable(false);
+        obj.setVisible(true);
+        obj.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        obj.add(gamePlay);
+    }
+}
